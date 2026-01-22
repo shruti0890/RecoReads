4c4a8d560d381834dd34425686b5be175d6af6bf
shruti0890 <shrutish863@gmail.com>
2026-01-22 20:49:26 +0530

    Remove hardcoded API key and use placeholder

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     var item, tile, author, publisher, bookLink, bookImg;
     var outputList = document.getElementById("list-output");
     var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
-    var apiKey = "key=AIzaSyDtXC7kb6a7xKJdm_Le6_BYoY5biz6s8Lw";
+    const API_KEY = "YOUR_API_KEY_HERE";
+;
     var placeHldr = '<img src="https://via.placeholder.com/150">';
     var searchData;
   
